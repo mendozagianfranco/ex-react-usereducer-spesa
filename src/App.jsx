2ac50d71953b82c45e7fe2d25582666d54c8ed3a
mendozagianfranco <mendozagianfranco88@gmail.com>
2025-07-04 15:36:45 +0200

    refactoring

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import ListProducts from './components/ListProducts';
 
 const products = [
@@ -9,26 +9,51 @@ const products = [
 ];
 
 
+
 function App() {
   const [addedProducts, SetAddedProducts] = useState([]);
   const totalPrice = addedProducts.reduce((acc, curr) => {
-    return acc += curr.price * curr.quantity;
+    return acc + (curr.price * curr.quantity);
   }, 0);
+
+  function addProduct(product) {
+    const findProduct = addedProducts.find(p => p.name === product.name);
+    if (findProduct) {
+      updateProductQuantity(findProduct.name, findProduct.quantity + 1);
+      return;
+    }
+    SetAddedProducts(curr => [...curr, { ...product, quantity: 1 }]);
+
+  }
+
+  function updateProductQuantity(name, quantity) {
+    SetAddedProducts(curr => curr.map(p => p.name === name ? { ...p, quantity } : p));
+  }
+
+  function removeFromCart(product) {
+    SetAddedProducts(curr => curr.filter(p => p.name !== product.name));
+  }
+
 
   return (
     <>
       <h1>Lista Prodotti</h1>
-      <ListProducts products={products} addedProducts={addedProducts} SetAddedProducts={SetAddedProducts} />
+      <ListProducts products={products} addProduct={addProduct} removeFromCart={removeFromCart} />
 
-      <h2>List dei prodotti nel carrello</h2>
-      <div>Totale nel carrello: {totalPrice}</div>
-      {addedProducts.map((p, index) => (
-        <div key={index}>
-          <h3>{p.name}</h3>
-          <p>Price: {p.price}</p>
-          <p>Quantity: {p.quantity}</p>
-        </div>
-      ))}
+      {addedProducts.length > 0 && (
+        <>
+          <h2>List dei prodotti nel carrello</h2>
+          <div>Totale nel carrello: {totalPrice.toFixed(2)} €</div>
+          {addedProducts.map((p, index) => (
+            <div key={index}>
+              <h3>{p.name}</h3>
+              <p>Price: {p.price.toFixed(2)} €</p>
+              <p>Quantity: {p.quantity}</p>
+              <button onClick={() => removeFromCart(p)}>Rimuovi dal carrello</button>
+            </div>
+          ))}
+        </>)
+      }
     </>
   );
 }
--- src/components/ListProducts.jsx
+++ src/components/ListProducts.jsx
@@ -1,31 +1,14 @@
-export default function ListProducts({ products, SetAddedProducts, addedProducts }) {
+export default function ListProducts({ products, addProduct }) {
 
-    function addProduct(product) {
-        const findProduct = addedProducts.find(p => p.name === product.name);
-        if (findProduct) {
-            updateProductQuantity(findProduct);
-        } else {
-
-            SetAddedProducts(curr => [...curr, { ...product, quantity: 1 }]);
-        }
-    }
-
-    function updateProductQuantity(product) {
-        product.quantity += 1;
-        SetAddedProducts(curr => curr.map(p => p));
-    }
-    function removeFromCart(product) {
-        SetAddedProducts(curr => curr.filter(p => p.name !== product.name));
-    }
 
     return (
         <div>
             {products.map((product, index) => (
                 <div key={index}>
                     <h2>{product.name}</h2>
-                    <p>Price: {product.price}</p>
+                    <p>Price: {product.price.toFixed(2)} €</p>
                     <button onClick={() => addProduct(product)}>Aggiungi al carrello</button>
-                    <button onClick={() => removeFromCart(product)}>Rimuovi dal carrello</button>
+
                 </div>
             ))}
         </div>
